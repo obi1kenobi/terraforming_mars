6698aee43ce24380645fa6272aacd725ca98ec15
Predrag Gruevski <obi1kenobi82@gmail.com>
2021-05-14 16:09:24 -0400

    Add a function stub for getting possible plays.

--- src/main.rs
+++ src/main.rs
@@ -315,6 +315,15 @@ pub(crate) enum PlayerTurn {
     Pass,
 }
 
+pub(crate) fn get_possible_generation_plays(
+    initial_state: &PlayerState,
+    offered_cards: Vec<Card>,
+) -> Vec<(Vec<Card>, Vec<PlayerTurn>, PlayerState)> {
+    // return vec of (unbought cards, taken turns, final state)
+
+    vec![(offered_cards, vec![PlayerTurn::Pass], initial_state.clone())]
+}
+
 pub fn main() {}
 
 #[cfg(test)]
