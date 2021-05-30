4086d940f73933edf1b068eeba59a004645578ac
Predrag Gruevski <obi1kenobi82@gmail.com>
2021-05-30 18:12:27 -0400

    Refactor tile locations to use explicit OnMars/OffMars enum variants.

--- src/board.rs
+++ src/board.rs
@@ -89,10 +89,15 @@ impl From<(isize, isize)> for Coordinates {
     }
 }
 
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub enum TileLocation {
+    OnMars(Coordinates),
+    OffMars(SpecialLocation),
+}
+
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct MarsBoard {
-    pub mars_tiles: BTreeMap<Coordinates, BoardTile>,
-    pub special_tiles: Vec<BoardTile>,
+    pub tiles: BTreeMap<TileLocation, BoardTile>,
     pub placed_oceans: usize,
     pub oxygen: usize,
     pub temperature: isize,
@@ -100,15 +105,13 @@ pub struct MarsBoard {
 
 impl MarsBoard {
     pub fn new(
-        mars_tiles: BTreeMap<Coordinates, BoardTile>,
-        special_tiles: Vec<BoardTile>,
+        tiles: BTreeMap<TileLocation, BoardTile>,
         placed_oceans: usize,
         oxygen: usize,
         temperature: isize,
     ) -> MarsBoard {
         MarsBoard {
-            mars_tiles,
-            special_tiles,
+            tiles,
             placed_oceans,
             oxygen,
             temperature,
@@ -128,7 +131,7 @@ pub enum TileDesignation {
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct BoardTile {
     pub name: Option<String>,
-    pub mars_coordinates: Option<Coordinates>, // non-Mars tiles don't have Mars coordinates
+    pub location: TileLocation,
     pub designations: Vec<TileDesignation>,
     pub placement_bonus: Vec<ImmediateImpact>,
 }
@@ -136,38 +139,38 @@ pub struct BoardTile {
 impl BoardTile {
     pub fn new(
         name: Option<String>,
-        mars_coordinates: Option<Coordinates>,
+        location: TileLocation,
         designations: Vec<TileDesignation>,
         placement_bonus: Vec<ImmediateImpact>,
-    ) -> BoardTile {
+    ) -> Self {
         BoardTile {
             name,
-            mars_coordinates,
+            location,
             designations,
             placement_bonus,
         }
     }
 
     #[inline]
-    pub fn new_unnamed<CoordT: Into<Coordinates>>(
+    pub fn new_on_mars<CoordT: Into<Coordinates>>(
         mars_coordinates: CoordT,
         designations: Vec<TileDesignation>,
         placement_bonus: Vec<ImmediateImpact>,
     ) -> BoardTile {
         BoardTile::new(
             None,
-            Some(mars_coordinates.into()),
+            TileLocation::OnMars(mars_coordinates.into()),
             designations,
             placement_bonus,
         )
     }
 
     #[inline]
-    pub fn new_unnamed_land<CoordT: Into<Coordinates>>(
+    pub fn new_land_on_mars<CoordT: Into<Coordinates>>(
         mars_coordinates: CoordT,
         placement_bonus: Vec<ImmediateImpact>,
     ) -> BoardTile {
-        BoardTile::new_unnamed(
+        BoardTile::new_on_mars(
             mars_coordinates,
             vec![TileDesignation::Land],
             placement_bonus,
@@ -175,65 +178,66 @@ impl BoardTile {
     }
 
     #[inline]
-    pub fn new_unnamed_non_bonus_land<CoordT: Into<Coordinates>>(
+    pub fn new_non_bonus_land_on_mars<CoordT: Into<Coordinates>>(
         mars_coordinates: CoordT,
     ) -> BoardTile {
-        BoardTile::new_unnamed_land(mars_coordinates, vec![])
+        BoardTile::new_land_on_mars(mars_coordinates, vec![])
     }
 
     #[inline]
-    pub fn new_unnamed_resource_bonus_land<CoordT: Into<Coordinates>>(
+    pub fn new_resource_bonus_land_on_mars<CoordT: Into<Coordinates>>(
         mars_coordinates: CoordT,
         resource: Resource,
         count: usize,
     ) -> BoardTile {
-        BoardTile::new_unnamed_land(
+        BoardTile::new_land_on_mars(
             mars_coordinates,
             vec![ImmediateImpact::GainResource(resource, count)],
         )
     }
 
     #[inline]
-    pub fn new_unnamed_card_draw_land<CoordT: Into<Coordinates>>(
+    pub fn new_card_draw_land_on_mars<CoordT: Into<Coordinates>>(
         mars_coordinates: CoordT,
         card_count: usize,
     ) -> BoardTile {
-        BoardTile::new_unnamed_land(
+        BoardTile::new_land_on_mars(
             mars_coordinates,
             vec![ImmediateImpact::DrawCard(card_count)],
         )
     }
 
     #[inline]
-    pub fn new_unnamed_ocean<CoordT: Into<Coordinates>>(
+    pub fn new_ocean_on_mars<CoordT: Into<Coordinates>>(
         mars_coordinates: CoordT,
         placement_bonus: Vec<ImmediateImpact>,
     ) -> BoardTile {
-        BoardTile::new_unnamed(
-            mars_coordinates,
+        BoardTile::new(
+            None,
+            TileLocation::OnMars(mars_coordinates.into()),
             vec![TileDesignation::ReservedForOcean],
             placement_bonus,
         )
     }
 
     #[inline]
-    pub fn new_unnamed_resource_bonus_ocean<CoordT: Into<Coordinates>>(
+    pub fn new_resource_bonus_ocean_on_mars<CoordT: Into<Coordinates>>(
         mars_coordinates: CoordT,
         resource: Resource,
         count: usize,
     ) -> BoardTile {
-        BoardTile::new_unnamed_ocean(
+        BoardTile::new_ocean_on_mars(
             mars_coordinates,
             vec![ImmediateImpact::GainResource(resource, count)],
         )
     }
 
     #[inline]
-    pub fn new_unnamed_card_draw_ocean<CoordT: Into<Coordinates>>(
+    pub fn new_card_draw_ocean_on_mars<CoordT: Into<Coordinates>>(
         mars_coordinates: CoordT,
         card_count: usize,
     ) -> BoardTile {
-        BoardTile::new_unnamed_ocean(
+        BoardTile::new_ocean_on_mars(
             mars_coordinates,
             vec![ImmediateImpact::DrawCard(card_count)],
         )
@@ -244,7 +248,7 @@ pub fn make_standard_non_mars_tiles() -> Vec<BoardTile> {
     vec![
         BoardTile::new(
             Some("Phobos Space Haven".into()),
-            None,
+            TileLocation::OffMars(SpecialLocation::PhobosSpaceHaven),
             vec![
                 TileDesignation::NonMarsTile,
                 TileDesignation::Special(SpecialLocation::PhobosSpaceHaven),
@@ -253,7 +257,7 @@ pub fn make_standard_non_mars_tiles() -> Vec<BoardTile> {
         ),
         BoardTile::new(
             Some("Ganymede Colony".into()),
-            None,
+            TileLocation::OffMars(SpecialLocation::GanymedeColony),
             vec![
                 TileDesignation::NonMarsTile,
                 TileDesignation::Special(SpecialLocation::GanymedeColony),
@@ -264,16 +268,16 @@ pub fn make_standard_non_mars_tiles() -> Vec<BoardTile> {
 }
 
 pub fn make_standard_game_board() -> MarsBoard {
-    let special_tiles = make_standard_non_mars_tiles();
     let placed_oceans = 0usize;
     let oxygen = 0usize;
     let temperature = -30isize;
 
-    let mut mars_tiles: Vec<BoardTile> = vec![
+    let mut tiles = make_standard_non_mars_tiles();
+    tiles.extend_from_slice(&[
         // top-left edge = first top-rightward column
         BoardTile::new(
             Some("Arsia Mons".into()),
-            Some((0, 0).into()),
+            TileLocation::OnMars((0, 0).into()),
             vec![
                 TileDesignation::Land,
                 TileDesignation::Special(SpecialLocation::VolcanicArea),
@@ -282,7 +286,7 @@ pub fn make_standard_game_board() -> MarsBoard {
         ),
         BoardTile::new(
             Some("Pavonis Mons".into()),
-            Some((1, 0).into()),
+            TileLocation::OnMars((1, 0).into()),
             vec![
                 TileDesignation::Land,
                 TileDesignation::Special(SpecialLocation::VolcanicArea),
@@ -294,111 +298,110 @@ pub fn make_standard_game_board() -> MarsBoard {
         ),
         BoardTile::new(
             Some("Ascraeus Mons".into()),
-            Some((2, 0).into()),
+            TileLocation::OnMars((2, 0).into()),
             vec![
                 TileDesignation::Land,
                 TileDesignation::Special(SpecialLocation::VolcanicArea),
             ],
             vec![ImmediateImpact::DrawCard(1)],
         ),
-        BoardTile::new_unnamed_non_bonus_land((3, 0)),
-        BoardTile::new_unnamed_resource_bonus_land((4, 0), Resource::Steel, 2),
+        BoardTile::new_non_bonus_land_on_mars((3, 0)),
+        BoardTile::new_resource_bonus_land_on_mars((4, 0), Resource::Steel, 2),
         //
         // second top-rightward column
-        BoardTile::new_unnamed_resource_bonus_land((0, -1), Resource::Plants, 1),
-        BoardTile::new_unnamed_resource_bonus_land((1, -1), Resource::Plants, 2),
-        BoardTile::new_unnamed_resource_bonus_land((2, -1), Resource::Plants, 1),
-        BoardTile::new_unnamed_non_bonus_land((3, -1)),
+        BoardTile::new_resource_bonus_land_on_mars((0, -1), Resource::Plants, 1),
+        BoardTile::new_resource_bonus_land_on_mars((1, -1), Resource::Plants, 2),
+        BoardTile::new_resource_bonus_land_on_mars((2, -1), Resource::Plants, 1),
+        BoardTile::new_non_bonus_land_on_mars((3, -1)),
         BoardTile::new(
             Some("Tharsis Tholus".into()),
-            Some((4, -1).into()),
+            TileLocation::OnMars((4, -1).into()),
             vec![
                 TileDesignation::Land,
                 TileDesignation::Special(SpecialLocation::VolcanicArea),
             ],
             vec![ImmediateImpact::GainResource(Resource::Steel, 1)],
         ),
-        BoardTile::new_unnamed_resource_bonus_ocean((5, -1), Resource::Steel, 2),
+        BoardTile::new_resource_bonus_ocean_on_mars((5, -1), Resource::Steel, 2),
         //
         // third top-rightward column
-        BoardTile::new_unnamed_non_bonus_land((0, -2)),
-        BoardTile::new_unnamed_resource_bonus_land((1, -2), Resource::Plants, 2),
+        BoardTile::new_non_bonus_land_on_mars((0, -2)),
+        BoardTile::new_resource_bonus_land_on_mars((1, -2), Resource::Plants, 2),
         BoardTile::new(
             Some("Noctis City".into()),
-            Some((2, -2).into()),
+            TileLocation::OnMars((2, -2).into()),
             vec![
                 TileDesignation::Land,
                 TileDesignation::Special(SpecialLocation::NoctisCity),
             ],
             vec![ImmediateImpact::GainResource(Resource::Plants, 2)],
         ),
-        BoardTile::new_unnamed_resource_bonus_land((3, -2), Resource::Plants, 1),
-        BoardTile::new_unnamed_non_bonus_land((4, -2)),
-        BoardTile::new_unnamed_non_bonus_land((5, -2)),
-        BoardTile::new_unnamed_non_bonus_land((6, -2)),
+        BoardTile::new_resource_bonus_land_on_mars((3, -2), Resource::Plants, 1),
+        BoardTile::new_non_bonus_land_on_mars((4, -2)),
+        BoardTile::new_non_bonus_land_on_mars((5, -2)),
+        BoardTile::new_non_bonus_land_on_mars((6, -2)),
         //
         // fourth top-rightward column
-        BoardTile::new_unnamed_resource_bonus_land((0, -3), Resource::Steel, 2),
-        BoardTile::new_unnamed_non_bonus_land((1, -3)),
-        BoardTile::new_unnamed_resource_bonus_land((2, -3), Resource::Plants, 1),
-        BoardTile::new_unnamed_resource_bonus_ocean((3, -3), Resource::Plants, 2),
-        BoardTile::new_unnamed_resource_bonus_land((4, -3), Resource::Plants, 1),
-        BoardTile::new_unnamed_non_bonus_land((5, -3)),
-        BoardTile::new_unnamed_non_bonus_land((6, -3)),
-        BoardTile::new_unnamed_card_draw_ocean((7, -3), 1),
+        BoardTile::new_resource_bonus_land_on_mars((0, -3), Resource::Steel, 2),
+        BoardTile::new_non_bonus_land_on_mars((1, -3)),
+        BoardTile::new_resource_bonus_land_on_mars((2, -3), Resource::Plants, 1),
+        BoardTile::new_resource_bonus_ocean_on_mars((3, -3), Resource::Plants, 2),
+        BoardTile::new_resource_bonus_land_on_mars((4, -3), Resource::Plants, 1),
+        BoardTile::new_non_bonus_land_on_mars((5, -3)),
+        BoardTile::new_non_bonus_land_on_mars((6, -3)),
+        BoardTile::new_card_draw_ocean_on_mars((7, -3), 1),
         //
         // fifth top-rightward column -- bottom-left map corner
-        BoardTile::new_unnamed_resource_bonus_land((0, -4), Resource::Steel, 1),
-        BoardTile::new_unnamed_non_bonus_land((1, -4)),
-        BoardTile::new_unnamed_non_bonus_land((2, -4)),
-        BoardTile::new_unnamed_resource_bonus_land((3, -4), Resource::Plants, 1),
-        BoardTile::new_unnamed_resource_bonus_ocean((4, -4), Resource::Plants, 2),
-        BoardTile::new_unnamed_resource_bonus_land((5, -4), Resource::Plants, 2),
-        BoardTile::new_unnamed_non_bonus_land((6, -4)),
-        BoardTile::new_unnamed_non_bonus_land((7, -4)),
-        BoardTile::new_unnamed_ocean((8, -4), vec![]),
+        BoardTile::new_resource_bonus_land_on_mars((0, -4), Resource::Steel, 1),
+        BoardTile::new_non_bonus_land_on_mars((1, -4)),
+        BoardTile::new_non_bonus_land_on_mars((2, -4)),
+        BoardTile::new_resource_bonus_land_on_mars((3, -4), Resource::Plants, 1),
+        BoardTile::new_resource_bonus_ocean_on_mars((4, -4), Resource::Plants, 2),
+        BoardTile::new_resource_bonus_land_on_mars((5, -4), Resource::Plants, 2),
+        BoardTile::new_non_bonus_land_on_mars((6, -4)),
+        BoardTile::new_non_bonus_land_on_mars((7, -4)),
+        BoardTile::new_ocean_on_mars((8, -4), vec![]),
         //
         // sixth top-rightward column -- starts with second tile on the bottom edge
-        BoardTile::new_unnamed_resource_bonus_land((1, -5), Resource::Steel, 2),
-        BoardTile::new_unnamed_non_bonus_land((2, -5)),
-        BoardTile::new_unnamed_non_bonus_land((3, -5)),
-        BoardTile::new_unnamed_resource_bonus_land((4, -5), Resource::Plants, 1),
-        BoardTile::new_unnamed_resource_bonus_ocean((5, -5), Resource::Plants, 2),
-        BoardTile::new_unnamed_resource_bonus_land((6, -5), Resource::Plants, 1),
-        BoardTile::new_unnamed_non_bonus_land((7, -5)),
-        BoardTile::new_unnamed_card_draw_ocean((8, -5), 2),
+        BoardTile::new_resource_bonus_land_on_mars((1, -5), Resource::Steel, 2),
+        BoardTile::new_non_bonus_land_on_mars((2, -5)),
+        BoardTile::new_non_bonus_land_on_mars((3, -5)),
+        BoardTile::new_resource_bonus_land_on_mars((4, -5), Resource::Plants, 1),
+        BoardTile::new_resource_bonus_ocean_on_mars((5, -5), Resource::Plants, 2),
+        BoardTile::new_resource_bonus_land_on_mars((6, -5), Resource::Plants, 1),
+        BoardTile::new_non_bonus_land_on_mars((7, -5)),
+        BoardTile::new_card_draw_ocean_on_mars((8, -5), 2),
         //
         // seventh top-rightward column -- starts with third tile on the bottom edge
-        BoardTile::new_unnamed_non_bonus_land((2, -6)),
-        BoardTile::new_unnamed_card_draw_land((3, -6), 1),
-        BoardTile::new_unnamed_non_bonus_land((4, -6)),
-        BoardTile::new_unnamed_resource_bonus_ocean((5, -6), Resource::Plants, 1),
-        BoardTile::new_unnamed_resource_bonus_land((6, -6), Resource::Plants, 2),
-        BoardTile::new_unnamed_resource_bonus_land((7, -6), Resource::Plants, 1),
-        BoardTile::new_unnamed_resource_bonus_land((8, -6), Resource::Steel, 1),
+        BoardTile::new_non_bonus_land_on_mars((2, -6)),
+        BoardTile::new_card_draw_land_on_mars((3, -6), 1),
+        BoardTile::new_non_bonus_land_on_mars((4, -6)),
+        BoardTile::new_resource_bonus_ocean_on_mars((5, -6), Resource::Plants, 1),
+        BoardTile::new_resource_bonus_land_on_mars((6, -6), Resource::Plants, 2),
+        BoardTile::new_resource_bonus_land_on_mars((7, -6), Resource::Plants, 1),
+        BoardTile::new_resource_bonus_land_on_mars((8, -6), Resource::Steel, 1),
         //
         // eighth top-rightward column -- starts with fourth tile on the bottom edge
-        BoardTile::new_unnamed_non_bonus_land((3, -7)),
-        BoardTile::new_unnamed_non_bonus_land((4, -7)),
-        BoardTile::new_unnamed_resource_bonus_land((5, -7), Resource::Plants, 1),
-        BoardTile::new_unnamed_resource_bonus_ocean((6, -7), Resource::Plants, 1),
-        BoardTile::new_unnamed_resource_bonus_land((7, -7), Resource::Plants, 2),
-        BoardTile::new_unnamed_resource_bonus_ocean((8, -7), Resource::Plants, 2),
+        BoardTile::new_non_bonus_land_on_mars((3, -7)),
+        BoardTile::new_non_bonus_land_on_mars((4, -7)),
+        BoardTile::new_resource_bonus_land_on_mars((5, -7), Resource::Plants, 1),
+        BoardTile::new_resource_bonus_ocean_on_mars((6, -7), Resource::Plants, 1),
+        BoardTile::new_resource_bonus_land_on_mars((7, -7), Resource::Plants, 2),
+        BoardTile::new_resource_bonus_ocean_on_mars((8, -7), Resource::Plants, 2),
         //
         // last top-rightward column -- lower-right edge
-        BoardTile::new_unnamed_resource_bonus_ocean((4, -8), Resource::Titanium, 2),
-        BoardTile::new_unnamed_resource_bonus_land((5, -8), Resource::Titanium, 1),
-        BoardTile::new_unnamed_non_bonus_land((6, -8)),
-        BoardTile::new_unnamed_resource_bonus_ocean((7, -8), Resource::Plants, 1),
-        BoardTile::new_unnamed_resource_bonus_land((8, -8), Resource::Plants, 2),
-    ];
+        BoardTile::new_resource_bonus_ocean_on_mars((4, -8), Resource::Titanium, 2),
+        BoardTile::new_resource_bonus_land_on_mars((5, -8), Resource::Titanium, 1),
+        BoardTile::new_non_bonus_land_on_mars((6, -8)),
+        BoardTile::new_resource_bonus_ocean_on_mars((7, -8), Resource::Plants, 1),
+        BoardTile::new_resource_bonus_land_on_mars((8, -8), Resource::Plants, 2),
+    ]);
 
     MarsBoard::new(
-        mars_tiles
+        tiles
             .drain(..)
-            .map(|tile| (tile.mars_coordinates.unwrap(), tile))
+            .map(|tile| (tile.location.clone(), tile))
             .collect(),
-        special_tiles,
         placed_oceans,
         oxygen,
         temperature,
